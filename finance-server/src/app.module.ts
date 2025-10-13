ad16f89c2b5ae89f65166af73fc76e18bec8ce5b
Weegoos <batyr.ashim05@mail.ru>
2025-10-13 20:18:45 +0200

    Jotter-finance dev-version 1.0.1: The User table has been added

--- finance-server/src/app.module.ts
+++ finance-server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users/user.model';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      database: 'postgres',
+      models: [User],
+      username: 'postgres',
+      password: '2005',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
