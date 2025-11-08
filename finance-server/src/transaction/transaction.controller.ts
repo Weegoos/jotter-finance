4a96da93baac925d32934f35a949d62424d9f3dc
Weegoos <batyr.ashim05@mail.ru>
2025-11-08 19:46:05 +0100

    Jotter-finance: New features related to transaction have been added

--- finance-server/src/transaction/transaction.controller.ts
+++ finance-server/src/transaction/transaction.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import {
   ApiBearerAuth,
   ApiOperation,
@@ -26,7 +35,7 @@ export class TransactionController {
   @ApiResponse({ status: 201, description: 'Transaction created successfully' })
   @ApiResponse({ status: 400, description: 'Bad Request' })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
-  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 500, description: 'Current Account does not exist' })
   async create(
     @Req() req: any,
     @Body() transaction: CreateTransactionDTO,
@@ -37,5 +46,26 @@ export class TransactionController {
     );
 
     return newTransaction;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @Get()
+  @ApiOperation({ summary: 'Get all transactions for a user' })
+  @ApiResponse({ status: 201, description: 'Transaction created successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  async findAllTransactions(@Req() req: any): Promise<ITransaction[]> {
+    return this.transactionService.findAll(req.user.id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete an transaction by ID' })
+  @ApiResponse({ status: 200, description: 'Account deleted successfully' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  async delete(@Req() req: any, @Param('id') id: number): Promise<void> {
+    return this.transactionService.destroy(id, req.user.id);
   }
 }
--- finance-server/src/transaction/transaction.module.ts
+++ finance-server/src/transaction/transaction.module.ts
@@ -5,9 +5,11 @@ import { TransactionService } from './transaction.service';
 import { JwtStrategy } from 'src/auth/jwt.strategy';
 import { ChatGateway } from 'src/chat.gateway';
 import { TransactionController } from './transaction.controller';
+import { Account } from 'src/accounts/account.model';
+import { Categories } from 'src/categories/categories.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Transactions])],
+  imports: [SequelizeModule.forFeature([Transactions, Account, Categories])],
   providers: [TransactionService, JwtStrategy, ChatGateway],
   exports: [TransactionService],
   controllers: [TransactionController],
